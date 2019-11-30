ea46b23ca8909a822161958d56d5c62b23e86123
aliev <sambufer200@gmail.com>
2019-11-30 23:29:17 +0300

    edited connection class and added some methods

--- src/network/Connection.java
+++ src/network/Connection.java
@@ -19,9 +19,9 @@ public class Connection {
         start(IP, PORT);
     }
 
-    public void start(String ip, int port) {
+    private void start(String ip, int port) {
+        Scanner scanner = new Scanner(System.in);
         try {
-            Scanner scanner = new Scanner(System.in);
             System.out.println("Введите ip адрес сервера: ");
             ip = scanner.nextLine();
             Socket socket = new Socket(ip, port);
@@ -33,4 +33,23 @@ public class Connection {
 
     }
 
+    public BufferedReader getBf() {
+        return bf;
+    }
+
+    public void setBf(BufferedReader bf) {
+        this.bf = bf;
+    }
+
+    public PrintWriter getWriter() {
+        return writer;
+    }
+
+    public void setWriter(PrintWriter writer) {
+        this.writer = writer;
+    }
+
+    public void writeMessage(String message) {
+        this.writer.println(message);
+    }
 }
