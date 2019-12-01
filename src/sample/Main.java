15ba3dd7da4b15c4ee3d15c4606c1e24cf05f578
aliev <sambufer200@gmail.com>
2019-12-01 19:57:36 +0300

    added buttons in fmxl for game

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,20 +1,34 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene = new Scene(new Group(), 100, 100);
+
+        primaryStage.setOnCloseRequest(evt -> {
+            // allow user to decide between yes and no
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Do you really want to close this application?", ButtonType.YES, ButtonType.NO);
+
+            // clicking X also means no
+            ButtonType result = alert.showAndWait().orElse(ButtonType.NO);
+
+            if (ButtonType.NO.equals(result)) {
+                // consume event i.e. ignore close request
+                evt.consume();
+            }
+        });
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
+
 
 
     public static void main(String[] args) {
