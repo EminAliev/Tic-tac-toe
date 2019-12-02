7f39e96ed50ea3e8a0670e7e3fd1697e377248d2
aliev <sambufer200@gmail.com>
2019-12-03 02:06:19 +0300

    fixed main class

--- src/app/Main.java
+++ src/app/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("board.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/board.fxml"));
         primaryStage.setTitle("Tic tac toe");
         primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
